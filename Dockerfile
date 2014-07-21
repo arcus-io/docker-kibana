d450cdfc817d53a1cbd9374b651525c4ce97c240
Evan Hazlett <ejhazlett@gmail.com>
2014-07-21 16:43:03 -0400

    added run script to support kibana customization

--- Dockerfile
+++ Dockerfile
@@ -5,5 +5,6 @@ RUN apt-get install -y nginx-full wget
 RUN wget https://download.elasticsearch.org/kibana/kibana/kibana-3.1.0.tar.gz -O /tmp/kibana.tar.gz && \
     tar zxf /tmp/kibana.tar.gz && mv kibana-3.1.0/* /usr/share/nginx/html
 RUN echo "daemon off;" >> /etc/nginx/nginx.conf
+ADD run.sh /usr/local/bin/run
 EXPOSE 80
-CMD ["nginx", "-c", "/etc/nginx/nginx.conf"]
+CMD ["/usr/local/bin/run"]
---
+++ run.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+ES_HOST=${ES_HOST:-window.location.hostname}
+ES_PORT=${ES_PORT:-9200}
+
+cat << EOF > /usr/share/nginx/html/config.js
+define(['settings'],
+function (Settings) {
+  return new Settings({
+    elasticsearch: "http://"+$ES_HOST+":$ES_PORT",
+    default_route     : '/dashboard/file/logstash.json',
+    kibana_index: "kibana-int",
+    panel_names: [
+      'histogram',
+      'map',
+      'goal',
+      'table',
+      'filtering',
+      'timepicker',
+      'text',
+      'hits',
+      'column',
+      'trends',
+      'bettermap',
+      'query',
+      'terms',
+      'stats',
+      'sparklines'
+    ]
+  });
+});
+EOF
+
+nginx -c /etc/nginx/nginx.conf
