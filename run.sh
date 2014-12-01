79be279c169b627825476c9b89362c9c0b220447
JustAdam <adamb@aller.no>
2014-12-01 14:27:47 +0100

    Copy Kibana to correct directory.

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ ES_HOST=${ES_HOST:-\"+window.location.hostname+\"}
 ES_PORT=${ES_PORT:-9200}
 ES_SCHEME=${ES_SCHEME:-http}
 
-cat << EOF > /usr/share/nginx/html/config.js
+cat << EOF > /var/www/html/config.js
 define(['settings'],
 function (Settings) {
   return new Settings({
