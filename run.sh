4e674d0bbec555f93d6bc49314b2477083d4a50d
Ahmet Demir <ahmet2mir+github@gmail.com>
2014-12-22 11:23:56 +0100

    Disable IPv6 support for Nginx
    On some servers, IPv6 is not available, so Nginx can't listen on  [::]:80 
    
    	2014/12/19 07:18:32 [emerg] 484#0: socket() [::]:80 failed (97: Address family not supported by protocol)
    
    I removed the line and the container start successfully.

--- run.sh
+++ run.sh
@@ -3,6 +3,11 @@ ES_HOST=${ES_HOST:-\"+window.location.hostname+\"}
 ES_PORT=${ES_PORT:-9200}
 ES_SCHEME=${ES_SCHEME:-http}
 
+# disable ipv6 support
+if [ ! -f /proc/net/if_inet6 ]; then
+  sed -e '/listen \[::\]:80/ s/^#*/#/' -i /etc/nginx/sites-enabled/*
+fi
+
 cat << EOF > /usr/share/nginx/html/config.js
 define(['settings'],
 function (Settings) {
